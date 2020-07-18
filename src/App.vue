a079544dfb8eefb7d5d2ddb3eb4d2e91c3c3aae6
2578313972 <2578313972@qq.com>
2020-07-18 14:57:10 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,62 @@
 <template>
   <div id="app">
-    <router-view />
+   <div id="main" style="width: 600px;height: 400px;"></div>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      winWidth:0,
+      myChart_1:null
+    }
+  },
+  mounted(){
+    this.myChart_1 = this.echarts.init(document.getElementById('main'));
+
+    let option = {
+    color: ['#3398DB'],
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: [
+        {
+            name: '直接访问',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+        }
+    ]
+};
+this.myChart_1.setOption(option);
+
+  }
+}
+</script>
 
 <style lang="less">
   body,div,a,p,ul,li{font-size: 14px;margin: 0;}
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
+// import echarts from 'echarts'
+
 Vue.config.productionTip = false
 
 new Vue({
